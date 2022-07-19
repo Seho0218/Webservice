cd0b7592ce1f505e74c84701c180178cf9c2cd76
Seho0218 <ghdtpgh8913@gmail.com>
2022-07-20 01:38:43 +0900

    study

--- 01_Java/basic01/src/again/dan.java
+++ 01_Java/basic01/src/again/dan.java
@@ -8,7 +8,7 @@ public class dan {
 				
 			for(int i=1; i<10 ;i+=3) {						
 				for(int j=i; j<i+3 ; j++){					
-					System.out.printf("==%d��==\t \n", j );
+					System.out.printf("==%d단==\t \n", j );
 															
 				for(int k=1; k<10 ; k++){
 							System.out.printf("%dx%d=%d\t ", j ,k, j*k);
